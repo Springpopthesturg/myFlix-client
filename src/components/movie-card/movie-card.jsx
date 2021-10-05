7cfd29d0fd3c77b7a7c352fe56f15f45563c8d3a
Springpopthesturg <79934328+Springpopthesturg@users.noreply.github.com>
2021-10-05 01:52:36 -0400

    keypress

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 export class MovieCard extends React.Component {
+
+  componentDidMount() {
+    document.addEventListener('keypress', event => {
+      console.log(event.key);
+    });
+  }
+  
   render() {
     const { movie, onMovieClick } = this.props;
 
