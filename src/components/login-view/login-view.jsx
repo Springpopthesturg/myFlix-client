dbf8479da2f95ed8f461d64ef73e76a269a7a3d7
Springpopthesturg <79934328+Springpopthesturg@users.noreply.github.com>
2021-10-19 21:45:44 -0400

    login update for 3.6

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 export function LoginView(props) {
+<<<<<<< Updated upstream
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
@@ -25,4 +26,43 @@ export function LoginView(props) {
             <button type="submit" onClick={handleSubmit}>Submit</button>
         </form>
     );
+=======
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  import axios from 'axios';
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  /* Send a request to the server for authentication */
+  axios.post('YOUR_API_URL/login', {
+    Username: username,
+    Password: password
+  })
+  .then(response => {
+    const data = response.data;
+    props.onLoggedIn(data);
+  })
+  .catch(e => {
+    console.log('no such user')
+  });
+};
+
+  return (
+    <Form>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Submit
+        </Button>
+    </Form>
+  )
+>>>>>>> Stashed changes
 }
